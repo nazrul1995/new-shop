adb9d86a739b40e917d782a3d49890518b00b554
nazrul islam <nazrulislamtube@gmail.com>
2025-10-21 10:35:46 +0600

    Header Complete

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { CiHeart } from 'react-icons/ci';
+import { IoCartOutline } from 'react-icons/io5';
 import { NavLink } from 'react-router';
 
 const Navbar = () => {
@@ -9,7 +11,7 @@ const Navbar = () => {
     </>
     return (
         <div className='bg-base-100 shadow-sm'>
-            <div className="navbar w-11/12 mx-auto ">
+            <div className="navbar w-11/12 mx-auto py-5">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -26,8 +28,24 @@ const Navbar = () => {
                 <div className="navbar-center hidden lg:flex">
                     {links}
                 </div>
-                <div className="navbar-end">
-                    <a className="btn">Button</a>
+                <div className="navbar-end gap-x-2.5">
+                    <label className="input w-[240px]">
+                        <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                            <g
+                                strokeLinejoin="round"
+                                strokeLinecap="round"
+                                strokeWidth="2.5"
+                                fill="none"
+                                stroke="currentColor"
+                            >
+                                <circle cx="11" cy="11" r="8"></circle>
+                                <path d="m21 21-4.3-4.3"></path>
+                            </g>
+                        </svg>
+                        <input type="search" required placeholder="Search" />
+                    </label>
+                    <CiHeart size={30}/>
+                    <IoCartOutline size={30} />
                 </div>
             </div>
         </div>
